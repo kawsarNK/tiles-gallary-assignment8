7c0e3ef698855a7424e9df5e2aa48c8b7d2fc117
kawsarNK <mdkawsarnk78@gmail.com>
2026-05-05 14:20:25 +0600

    Implement sign in page and Oauth

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -6,6 +6,7 @@ dns.setServers(["1.1.1.1", "8.8.8.8"]);
 import { betterAuth } from "better-auth";
 import { MongoClient } from "mongodb";
 import { mongodbAdapter } from "better-auth/adapters/mongodb";
+import { env } from "node:process";
 
 
 const client = new MongoClient(process.env.MONGODB_URI);
@@ -18,4 +19,10 @@ export const auth = betterAuth({
   emailAndPassword: {
     enabled: true,
   },
+  socialProviders:{
+    google:{
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET  
+    }
+  }
 });
